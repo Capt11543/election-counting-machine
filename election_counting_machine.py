57d559a136e403434e98eeab655ca8cac4594a0a
Capt11543 <contactcaptow@gmail.com>
2025-12-26 11:30:04 -0500

    if __name__ == "__main__":

--- election_counting_machine.py
+++ election_counting_machine.py
@@ -7,26 +7,31 @@ import seat_election as SeatElection
 import random
 
 
-# Get input from the user
-SIZE = int(input("Please enter the number of seats: ")) # Number of Winners
-filename = input("Please enter the path to the raw ballots: ")
-seed = int(input("Please provide with a seed: "))
-random.seed(seed)
+def main():
+    # Get input from the user
+    SIZE = int(input("Please enter the number of seats: ")) # Number of Winners
+    filename = input("Please enter the path to the raw ballots: ")
+    seed = int(input("Please provide with a seed: "))
+    random.seed(seed)
 
-# Parse ballots
-ballots, candidates, parties = Parser.parse_raw_ballots(filename)
+    # Parse ballots
+    ballots, candidates, parties = Parser.parse_raw_ballots(filename)
 
-# Run the STV process
-achieved_quota, eliminated = STV.run(SIZE, ballots, candidates)
-print("\nThe following candidates have achieved a quota after transfers: ")
-print(Candidate.names_in_list(achieved_quota, False, True))
+    # Run the STV process
+    achieved_quota, eliminated = STV.run(SIZE, ballots, candidates)
+    print("\nThe following candidates have achieved a quota after transfers: ")
+    print(Candidate.names_in_list(achieved_quota, False, True))
 
-print("\n---\n")
+    print("\n---\n")
 
-SeatAllocation.run(parties, SIZE, achieved_quota)
+    SeatAllocation.run(parties, SIZE, achieved_quota)
 
-elected = SeatElection.run(parties, achieved_quota, eliminated, True)
+    elected = SeatElection.run(parties, achieved_quota, eliminated, True)
 
-print("\nThe following candidates have been elected to Parliament:")
-print(Candidate.names_in_list(elected, True, False))
-print("Congratulations to the elected candidates!  Thank you for voting!")
+    print("\nThe following candidates have been elected to Parliament:")
+    print(Candidate.names_in_list(elected, True, False))
+    print("Congratulations to the elected candidates!  Thank you for voting!")
+
+
+if __name__ == "__main__":
+    main()
